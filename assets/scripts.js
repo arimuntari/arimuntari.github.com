79dd6312eefdca547e389b65c797ea7cf36f8cc0
Ari Muntari <ari.muntari1@gmail.com>
2019-09-11 13:44:55 +0700

    dicoding

---
+++ assets/scripts.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function(){
+     document.getElementById("facebook").onclick = function(){	 
+		 window.open("https://web.facebook.com/ari.muntari");
+	 };
+     document.getElementById("twitter").onclick = function(){		 
+		 alert("Link Belum Ada");
+	 };
+     document.getElementById("gmail").onclick = function(){		 
+		 alert("ari.muntari1@gmail.com");
+	 };
+});
